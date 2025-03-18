90e5dcd54c1738580d65029f2d4d625282566e29
shadiev <shadiev.965@outlook.com>
2025-03-18 22:04:05 +0300

    Потоковая передача и загрузка работают прекрасно

--- src/api.cpp
+++ src/api.cpp
@@ -1,9 +1,48 @@
 #include "api.h"
 #include <nlohmann/json.hpp>
-
-using json = nlohmann::json;
+#include <fstream>
 
 void setupRoutes(crow::SimpleApp& app, Database& db) {
+    using json = nlohmann::json;
+
+    CROW_ROUTE(app, "/stream/<int>")
+    ([&db](const crow::request& req, crow::response& res, int id){
+        Song song = db.getSongById(id);
+        if (song.id == -1) {
+            res.code = 404;
+            res.end("Song not found");
+            return;
+        }
+        res.set_header("Content-Type", "audio/mpeg");
+        std::ifstream file(song.file, std::ios::binary);
+        if (!file) {
+            res.code = 500;
+            res.end("Error opening file");
+            return;
+        }
+        res.body = std::string((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+        res.end();
+    });
+
+    CROW_ROUTE(app, "/download/<int>")
+    ([&db](const crow::request& req, crow::response& res, int id){
+        Song song = db.getSongById(id);
+        if (song.id == -1) {
+            res.code = 404;
+            res.end("Song not found");
+            return;
+        }
+        res.set_header("Content-Disposition", "attachment; filename=\"" + song.title + ".mp3\"");
+        std::ifstream file(song.file, std::ios::binary);
+        if (!file) {
+            res.code = 500;
+            res.end("Error opening file");
+            return;
+        }
+        res.body = std::string((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+        res.end();
+    });
+
     CROW_ROUTE(app, "/songs").methods(crow::HTTPMethod::GET)
     ([&db]() {
         auto songs = db.getAllSongs();
